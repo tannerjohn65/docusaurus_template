ac8a001ed89da6f43530c0fbb23af798302a600b
tannerjohn65 <tannerjohn65@gmail.com>
2022-06-20 20:09:52 +0900

    organization deleted

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,11 +15,6 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'tannerjohn65', // Usually your GitHub org/user name.
-  projectName: 'docusaurus_test', // Usually your repo name.
-
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
